c3afabb9d3604a0b6e2a5d08bbb8ee5c41046972
Karthik Palusa <karthik.palusa@gmail.com>
2017-12-26 20:25:42 -0700

    Added comments to the backend code.

--- backenddb.py
+++ backenddb.py
@@ -1,30 +1,44 @@
+# import sqlite3 framework - this enables database creation with python integration
+
 import sqlite3
+
+# create the variable class
 
 class Database:
 
+# creation of the base database
     def __init__(self,db):
         self.conn=sqlite3.connect(db)
         self.cur=self.conn.cursor()
         self.cur.execute("CREATE TABLE IF NOT EXISTS book(id INTEGER PRIMARY KEY,title text ,author text,schoolID INTEGER ,due INTEGER )")
         self.conn.commit()
 
+# create the proper layout for the insert function
     def insert(self,title,author,schoolID,due):
         self.cur.execute("INSERT into book VALUES (NULL,?,?,?,?)",(title,author,schoolID,due))
         self.conn.commit()
+
+# create the proper layout for the view function
 
     def view(self):
         self.cur.execute("SELECT * FROM book")
         rows=self.cur.fetchall()
         return rows
 
+# create the proper layout for the search command
+
     def search(self,title="",author="",schooID="",due=""):
         self.cur.execute("SELECT * FROM book where title=? OR author=? OR schooID=? OR due=?",(title,author,schoolID,due))
         rows=self.cur.fetchall()
         return rows
 
+# delete function execution from the database itself
+
     def delete(self,id):
         self.cur.execute("DELETE FROM book where id =?",(id,))
         self.conn.commit()
+
+# self-recurring update argument
 
     def update(self,id,author,title,schoolID,due):
         self.cur.execute("UPDATE book SET title=?,author=?,schoolID=?,due=? WHERE id=?",(title,author,schoolID,due,id))
